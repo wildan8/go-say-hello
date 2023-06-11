dbbb3a8e2ead5c3df9a6de62c9ed72db08f261a9
wildanjerry <wildanjerry65@gmail.com>
2023-06-11 12:10:58 +0700

    update say hello with parameter

--- say_hello.go
+++ say_hello.go
@@ -1,6 +1,6 @@
 package go_say_hello
 
-func Say_hello() string {
-	return "Update, hello world. sorry for interupt hahaha..."
+func Say_hello(nama string) string {
+	return "Update, hello" + nama + ". sorry for interupt hahaha..."
 
 }
